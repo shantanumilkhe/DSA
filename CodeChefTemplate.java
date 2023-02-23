0ec3f7740f5c7d6c5436f71475d54104858654e2
Shantanu Milkhe <102807647+shantanumilkhe@users.noreply.github.com>
2023-02-23 21:56:12 +0530

    merge sort

---
+++ CodeChefTemplate.java
@@ -0,0 +1,30 @@
+package DSA;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChefTemplate
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		try {
+		     Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            
+        int a = sc.nextInt();
+
+        
+        
+        } 
+		} catch(Exception e) {
+		    return;
+		}
+	}
+}
+
